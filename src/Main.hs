74181105e7bb5f0c523f27b44b338a7ca5f2dc71
Luc Tielen <luc.tielen@gmail.com>
2022-02-28 08:41:22 +0100

    Call into zig object from Haskell

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,31 @@
 module Main ( main ) where
 
 import Language.Souffle.Interpreted as Souffle
+import Foreign.C
+import Foreign.Ptr
+import Foreign.ForeignPtr
+import Control.Exception
+
+foreign import ccall "add" foreignAdd :: CInt -> CInt -> CInt
+
+data Example
+
+foreign import ccall "init_example" createExample :: IO (Ptr Example)
+foreign import ccall "&deinit_example" destroyExample :: FunPtr (Ptr Example -> IO ())
+foreign import ccall "do_stuff_example" doStuffExample :: Ptr Example -> CInt -> IO CBool
+
+mkExample :: IO (ForeignPtr Example)
+mkExample = mask_ $ do
+  ptr <- createExample
+  newForeignPtr destroyExample ptr
 
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = do
+  print $ foreignAdd 3 4
+
+  -- obj <- createExample
+  -- print =<< doStuffExample obj 100
+  -- destroyExample obj
+
+  ptr <- mkExample
+  withForeignPtr ptr $ \p -> print =<< doStuffExample p 10
