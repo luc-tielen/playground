2c5d029d87dc2c68806a46c216abab3d65f4f7e6
Luc Tielen <luc.tielen@gmail.com>
2023-02-05 08:32:05 +0100

    Add FixedBufferAllocator

--- src/allocator.h
+++ src/allocator.h
@@ -1,11 +1,16 @@
 #pragma once
+#include <array>
 #include <cassert>
+#include <cstdint>
 #include <sys/mman.h>
 
+using u8 = uint8_t;
+
+static constexpr u8 ALIGNMENT = 16;
+
 class PageAllocator {
-public:
-    void *allocate(size_t /* bytes */) {
-        void *memory = mmap(
+    u8* allocate(size_t /* bytes */) {
+        u8* memory = (u8*) mmap(
             nullptr, // TODO can we pass last allocated address + PAGE_SIZE into first argument for contiguous pages?
             PAGE_SIZE,
             PROT_READ | PROT_WRITE,
@@ -17,12 +22,36 @@ public:
         return memory;
     }
 
-    void deallocate(void *memory) {
+    void deallocate(u8* memory) {
         assert(memory);
         int result = munmap(memory, PAGE_SIZE);
         assert(result == 0);
     }
 
-private:
     static constexpr size_t PAGE_SIZE = 4096;
+};
+
+inline size_t make_aligned(size_t pos, u8 alignment) {
+    return pos + (alignment - 1) & (~ 0x0F);
+}
+
+template<size_t num_bytes>
+class alignas(ALIGNMENT) FixedBufferAllocator {
+    u8* allocate(size_t size) {
+        size_t next_pos = make_aligned(last_pos, ALIGNMENT);
+
+        if (next_pos + size >= num_bytes) {
+            return nullptr;
+        }
+
+        last_pos = next_pos + size;
+        return buf.at(next_pos);
+    }
+
+    void deallocate(u8* /* memory */) {
+        // nop
+    }
+
+    std::array<u8, num_bytes> buf;
+    size_t last_pos = 0;
 };
