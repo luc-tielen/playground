a0eb4f680e3ba4a5b9f4f39d456f15acd79e0e40
luc-tielen <luc.tielen@gmail.com>
2023-02-12 12:26:30 +0100

    Add extra mmap flags

--- src/allocator.h
+++ src/allocator.h
@@ -15,9 +15,9 @@ class PageAllocator {
     u8* allocate(size_t /* bytes */) {
         u8* memory = (u8*) mmap(
             nullptr, // TODO can we pass last allocated address + PAGE_SIZE into first argument for contiguous pages?
-            PAGE_SIZE,
+            PAGE_SIZE, // TODO allow multiple of page size by passing in bytes here, and doing an assert on bytes % PAGE_SIZE == 0?
             PROT_READ | PROT_WRITE,
-            MAP_ANON | MAP_NORESERVE, // TODO what other flags are interesting? MAP_HUGE_2MB, MAP_HUGE_1GB? requires more setup.. (hugetlbfs)
+            MAP_PRIVATE | MAP_ANON | MAP_NORESERVE, // TODO what other flags are interesting? MAP_HUGE_2MB, MAP_HUGE_1GB? requires more setup.. (hugetlbfs)
             -1,
             0
         );
@@ -31,7 +31,7 @@ class PageAllocator {
         assert(result == 0);
     }
 
-    static constexpr size_t PAGE_SIZE = 4096;
+    static constexpr size_t PAGE_SIZE = 4096; //64kB for WASM
 };
 
 inline size_t make_aligned(size_t pos, u8 alignment) {
@@ -108,3 +108,5 @@ class alignas(ALIGNMENT) FixedBufferAllocator {
     std::array<u8, num_bytes> buf;
     size_t last_pos = 0;
 };
+
+// TODO slab allocator
